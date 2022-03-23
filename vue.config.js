84025577b4eac2fa22bceb94b2837f479a346353
bacon <1805198416@qq.com>
2022-03-23 08:47:39 +0800

    初始化

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+module.exports = {
+  lintOnSave: false, // eslint-loader 是否在保存的时候检查
+}
